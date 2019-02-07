e0373a5971bc178e358db8974510cb9b44d4f69a
Nao Pross <naopross@thearcway.org>
2019-02-07 17:35:07 +0100

    Change public headers directory (include) structure
    Add a public header that includes the entire library "mm.hpp",
    and move mmvec to a subdirectory "mm".

---
+++ include/mm.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+/* MiniMath
+ * A mathematical vector library that (ab)uses modern C++ abstraction features.
+ * This header includes the entire library.
+ *
+ * Warning: having (ab)used a lot of templated code it is not recommended to
+ *          include everything as it may slow down the compiler.
+ */
+
+#include "mm/mmvec.hpp"
